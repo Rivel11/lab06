9b6d475f08578d0658bbc70d05f8928d0b612131
Rivel11 <email@mail.ru>
2024-05-30 16:07:17 +0300

    comment and codestyle patch1

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <string>
 
 int main() {
-   string name;   // name 
+   string name;             // name 
    std::cin >> name;    // input
    std::cout << "hello from " << name << endl;  // output
    return 0;
